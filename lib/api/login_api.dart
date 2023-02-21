7c6f39bdd74c99dbafc99e40610a3713dcd646e3
AbdulHannan-123 <ah6475774@gmail.com>
2023-02-22 01:58:22 +0500

    created the login api function for the user to enter in

---
+++ lib/api/login_api.dart
@@ -0,0 +1,35 @@
+import 'dart:ffi';
+
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:login_with_bloc/models.dart';
+
+@immutable 
+abstract class LoginApiProtocol {
+  
+  const LoginApiProtocol();
+
+  Future<LoginHandle?> login({
+    required String email,
+    required String password,
+  });
+
+} 
+
+@immutable 
+class LoginApi implements LoginApiProtocol {
+
+    //// implementing the singleton pattren
+  const LoginApi._sharedInstance();       // its like the initializer kind of... constructor
+  static const LoginApi _shared = LoginApi._sharedInstance();       // 
+  factory LoginApi.instance() => _shared;
+
+  @override
+  Future<LoginHandle?> login({
+    required String email,
+     required String password,
+     }) => Future.delayed(
+      const Duration(seconds: 2),
+      ()=> email == 'foo#bar.com' && password == 'foobar'
+     ).then((isLoggedin) => isLoggedin ? LoginHandle.foobar() : null);
+  
+}
