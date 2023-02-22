bc0cce661180113e4e54dbadd1ad4193421080ba
AbdulHannan-123 <ah6475774@gmail.com>
2023-02-23 01:47:49 +0500

    implements the state and the action for the app

--- lib/api/login_api.dart
+++ lib/api/login_api.dart
@@ -3,33 +3,29 @@ import 'dart:ffi';
 import 'package:flutter/foundation.dart' show immutable;
 import 'package:login_with_bloc/models.dart';
 
-@immutable 
+@immutable
 abstract class LoginApiProtocol {
-  
   const LoginApiProtocol();
 
   Future<LoginHandle?> login({
     required String email,
     required String password,
   });
+}
 
-} 
-
-@immutable 
+@immutable
 class LoginApi implements LoginApiProtocol {
-
-    //// implementing the singleton pattren
-  const LoginApi._sharedInstance();       // its like the initializer kind of... constructor
-  static const LoginApi _shared = LoginApi._sharedInstance();       // 
+  //// implementing the singleton pattren
+  const LoginApi._sharedInstance(); // its like the initializer kind of... constructor
+  static const LoginApi _shared = LoginApi._sharedInstance(); //
   factory LoginApi.instance() => _shared;
 
   @override
   Future<LoginHandle?> login({
     required String email,
-     required String password,
-     }) => Future.delayed(
-      const Duration(seconds: 2),
-      ()=> email == 'foo#bar.com' && password == 'foobar'
-     ).then((isLoggedin) => isLoggedin ? LoginHandle.foobar() : null);
-  
+    required String password,
+  }) =>
+      Future.delayed(const Duration(seconds: 2),
+              () => email == 'foo#bar.com' && password == 'foobar')
+          .then((isLoggedin) => isLoggedin ? LoginHandle.foobar() : null);
 }
---
+++ lib/bloc/app_state.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:login_with_bloc/models.dart';
+
+@immutable 
+class AppState {
+  final bool isLoading;
+  final LoginErrors? loginError;    // you are not get error every time or may be have a error while login but not everytime
+  final LoginHandle? loginHandle;    //user isnt always log in many timme it log out too or not logged in yet
+  final Iterable<Note>? fetchNotes;  ////or may be you dont have any notes
+
+   const AppState({required this.isLoading, this.loginError, this.loginHandle, this.fetchNotes});   
+
+
+  @override
+  String toString() =>{
+    'isLoading' : isLoading,
+    'loginError': loginError,
+    'loginHandle':loginHandle,
+    'fetchNotes' : fetchNotes,
+  }.toString();
+
+
+}
