56d8933f9b157e3bf50eb71f7189e82dd0a267f0
AbdulHannan-123 <ah6475774@gmail.com>
2023-02-24 02:29:54 +0500

    implementing the bloc to perform the states

---
+++ lib/bloc/app_bloc.dart
@@ -0,0 +1,69 @@
+import 'package:bloc/bloc.dart';
+import 'package:login_with_bloc/api/login_api.dart';
+import 'package:login_with_bloc/api/notes_api.dart';
+import 'package:login_with_bloc/bloc/actions.dart';
+import 'package:login_with_bloc/bloc/app_state.dart';
+import 'package:login_with_bloc/models.dart';
+
+class AppBloc extends Bloc<AppAction, AppState> {
+  final LoginApiProtocol loginapi;
+  final NoteApiProtocol notesapi;
+
+  AppBloc({
+    required this.loginapi,
+    required this.notesapi,
+  }) : super(const AppState.empty()) {
+    // is the initial state of our bloc
+    on<LoginAction>((event, emit) async {
+      // start loading
+      emit(
+        const AppState(
+            isLoading: true,
+            fetchNotes: null,
+            loginError: null,
+            loginHandle: null),
+      );
+      // log the user in
+      final loginhandle = await LoginApi.instance().login(
+        email: event.email,
+        password: event.password,
+      );
+      emit(AppState(
+          isLoading: false,
+          fetchNotes: null,
+          loginError: loginhandle == null ? LoginErrors.invlidHandle : null,
+          loginHandle: loginhandle));
+    });
+
+    on<LoadNotesAction>(
+      (event, emit) {
+        //start loading
+        emit(
+           AppState(
+            isLoading: true,
+            fetchNotes: null,
+            loginError: null,
+            loginHandle: state.loginHandle,    // the stae is showing that the bloc is holding the previous state
+          ),
+        );
+        // get the login handle
+        final loginhandle = state.loginHandle;
+        if (loginhandle != const LoginHandle.foobar()) {  // invalid login handle can not fetch null
+          emit(
+           AppState(
+            isLoading: false,
+            fetchNotes: null,
+            loginError: LoginErrors.invlidHandle,
+            loginHandle: loginhandle ,    
+          ),
+        );
+        return;
+        }
+          // we have valid login handle and want to fetch notes
+
+          
+      
+      },
+    );
+  }
+}
--- lib/bloc/app_state.dart
+++ lib/bloc/app_state.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/foundation.dart' show immutable;
 import 'package:login_with_bloc/models.dart';
 
-@immutable 
+@immutable
 class AppState {
   final bool isLoading;
-  final LoginErrors? loginError;    // you are not get error every time or may be have a error while login but not everytime
-  final LoginHandle? loginHandle;    //user isnt always log in many timme it log out too or not logged in yet
-  final Iterable<Note>? fetchNotes;  ////or may be you dont have any notes
+  final LoginErrors?
+      loginError; // you are not get error every time or may be have a error while login but not everytime
+  final LoginHandle?
+      loginHandle; //user isnt always log in many timme it log out too or not logged in yet
+  final Iterable<Note>? fetchNotes; ////or may be you dont have any notes
 
-   const AppState({required this.isLoading, this.loginError, this.loginHandle, this.fetchNotes});   
+  const AppState.empty()     // we start with this as our initial app state
+      : isLoading = false,
+        loginError = null,
+        fetchNotes = null,
+        loginHandle = null;
 
+  const AppState(
+      {required this.isLoading,
+      this.loginError,
+      this.loginHandle,
+      this.fetchNotes});
 
   @override
-  String toString() =>{
-    'isLoading' : isLoading,
-    'loginError': loginError,
-    'loginHandle':loginHandle,
-    'fetchNotes' : fetchNotes,
-  }.toString();
-
-
+  String toString() => {
+        'isLoading': isLoading,
+        'loginError': loginError,
+        'loginHandle': loginHandle,
+        'fetchNotes': fetchNotes,
+      }.toString();
 }
